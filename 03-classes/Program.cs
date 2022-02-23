a7b7ee85ceffd413ae9c4d45c4ba026c474e43bf
gprok <gprok60@gmail.com>
2022-02-23 20:05:58 +0200

    classes

---
+++ 03-classes/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace classes
+{
+    class Patient
+    {
+        public string Name { get; set; }
+        public int Ssn { get; set; }
+
+        private string clinic;
+       
+        public Patient(string name, int ssn)
+        {
+            Name = name;
+            Ssn = ssn;
+            clinic = null;
+        }
+
+        public Patient(Patient p) : this(p.Name, p.Ssn, p.GetClinic()) { }
+
+        public Patient(string name, int ssn, string clinic) : this(name, ssn)
+        {
+            this.clinic = clinic;
+        }
+
+        public bool AddToClinic(string clinic)
+        {
+            if (clinic == "A" || clinic == "B" ||  clinic == "C")
+            {
+                this.clinic = clinic;
+                return true;
+            }
+            else
+            {
+                this.clinic = null;
+                return false;
+            }
+        }
+
+        public bool AddToClinic(int c)
+        {
+            return true;
+        }
+
+        public void LeaveHospital()
+        {
+            this.clinic = null;
+        }
+
+        public string GetClinic()
+        {
+            if(this.clinic == null)
+            {
+                return "None";
+            }
+            else
+            {
+                return this.clinic;
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return "SSN: " + Ssn + ", " + Name + " clinic: " + GetClinic();
+        }
+
+        public void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine(a + ", " + b);
+        }
+
+        public void Swap( int a,  int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine(a + ", " + b);
+        }
+
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Patient p1 = new Patient("George", 111);
+            Patient p2 = new Patient("John", 222, "C");
+            Patient p3 = new Patient(p1);
+
+            p3.Name = "Bob";
+         
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
+
+            int x = 6;
+            int y = 7;
+            p1.Swap( x,  y);
+            Console.WriteLine(x + ", " + y);
+        }
+    }
+}
---
+++ 03-classes/README.md
@@ -0,0 +1,5 @@
+# Classes and Objects
+
+Here we create a very simple class ```Patient``` in order to understand the concepts of class variables and methods, properties, constructor, and overloading.
+
+There is also a method swap, irrelevant to the Patient which is used to demonstrate passing by value and passing by reference.
