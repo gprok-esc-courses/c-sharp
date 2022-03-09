d82812e9a5852727f9eb91cf66bdaf69e5482f05
gprok <gprok60@gmail.com>
2022-03-09 19:43:24 +0200

    inheritance

---
+++ 05-inheritance/animals/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace animals
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+
+        public Animal(string Name)
+        {
+            this.Name = Name;   
+        }
+
+        public virtual void Sound()
+        {
+            // No sound
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public Dog(string Name) : base(Name) {  }
+
+        public override void Sound()
+        {
+            Console.WriteLine("Wooof");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public Cat(string Name) : base(Name) { }
+
+        public override void Sound()
+        {
+            Console.WriteLine("Mieow");
+        }
+    }
+
+    public class Duck : Animal
+    {
+        public Duck(string Name) : base(Name) { }
+
+        public override void Sound()
+        {
+            Console.WriteLine("Quack");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal[] animals = new Animal[3];
+            animals[0] = new Dog("Pluto");
+            animals[1] = new Cat("Sylvester");
+            animals[2] = new Duck("Donald");
+
+            foreach(Animal animal in animals)
+            {
+                animal.Sound();
+            }
+            /*
+            foreach(Animal animal in animals)
+            {
+                if (animal.GetType().ToString() == "animals.Dog")
+                {
+                    Dog d = (Dog)animal;
+                    d.Sound();
+                }
+                else if (animal.GetType().ToString() == "animals.Cat")
+                {
+                    Cat c = (Cat)animal;
+                    c.Speak();
+                }
+                else if (animal.GetType().ToString() == "animals.Duck")
+                {
+                    Duck d = (Duck)animal;
+                    d.Quack();
+                }
+            }*/
+        }
+    }
+}
---
+++ 05-inheritance/person/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace oop
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        
+        public Person(string Name)
+        {
+            this.Name = Name;   
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    public class Student : Person
+    {
+        public string Major { get; set; }
+
+        public Student(string Name, string Major) : base(Name)
+        {
+            this.Major = Major;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + this.Major;
+        }
+    }
+
+    public class Teacher : Person
+    {
+        public string Room { get; set; }
+
+        public Teacher(string Name, string Room) : base(Name)
+        {
+            this.Room = Room;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + this.Room;
+        }
+
+        public void teach()
+        {
+            Console.Write("Teaching");
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Tom", "CS");
+
+            Person[] persons = new Person[5];
+            persons[0] = new Student("John", "IT");
+            persons[1] = new Teacher("Mike", "A5");
+
+            foreach(Person person in persons)
+            {
+                if (person != null)
+                {
+                    if(person.GetType().ToString() == "oop.Student")
+                    {
+                        Console.WriteLine("do student stuff");
+                    }
+                    else if (person.GetType().ToString() == "oop.Teacher")
+                    {
+                        Console.WriteLine("do teacher stuff");
+                    }
+                    
+                }
+            }
+
+            Console.WriteLine(student);
+        }
+    }
+}
