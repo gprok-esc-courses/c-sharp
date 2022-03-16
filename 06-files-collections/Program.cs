070f91066854615b43aeef57ba51c90667e690cd
gprok <gprok60@gmail.com>
2022-03-16 21:07:51 +0200

    files collections intro

---
+++ 06-files-collections/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace files
+{
+    public class Movie
+    {
+        public string Title { get; set; }   
+        public int InTopTen { get; set; } 
+
+        public Movie(string Title)
+        {
+            this.Title = Title;
+        }
+    }
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader(File.OpenRead("netflix.csv"));
+            List<Movie> Movies = new List<Movie>();
+
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                var values = line.Split(',');
+                Movie m = Movies.Find(x => x.Title == values[4]);
+                if (m == null)
+                {
+                    m = new Movie(values[4]);
+                    m.InTopTen = 1;
+                    Movies.Add(m);
+                }
+                else
+                {
+                    m.InTopTen++;
+                }
+                
+            }
+
+            foreach(var Movie in Movies)
+            {
+                Console.WriteLine(Movie.Title + " " + Movie.InTopTen);
+            }
+        }
+    }
+}
